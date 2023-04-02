bf21df9c4432a3a25e19f1f4579e06c27c253202
ColinChan520 <a1864874@adelaide.edu.au>
2023-04-02 20:23:05 +0930

    2rd commit

--- Finder.cpp
+++ Finder.cpp
@@ -9,17 +9,26 @@
         for(size_t i = 0; i <= s1.size(); i++) {
             int indexS1 = i;
             int indexS2 = 0;
+            bool allFound = false;
             while(s1[indexS1] == s2[indexS2]){
                 if(indexS2 + 1 > result.size()){
                     result.push_back(i);    
                 }
-                if(indexS1 + 1 == s1.length() || indexS2 + 1 == s2.length()){
+                if(indexS1 + 1 == s1.length()){
+                    break;
+                }
+                else if (indexS2 + 1 == s2.length())
+                {
+                    allFound = true;
                     break;
                 }
                 else{
                 indexS1++;
                 indexS2++;
                 }
+            }
+            if(allFound){
+                break;
             }
     }
     int returnNotFound = s2.length() - result.size();
