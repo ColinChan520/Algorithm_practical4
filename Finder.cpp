f17611e340d92125185bc2377890e1c43f9b87b7
ColinChan520 <a1864874@adelaide.edu.au>
2023-04-02 20:51:59 +0930

    6th commit

--- Finder.cpp
+++ Finder.cpp
@@ -5,27 +5,21 @@ using namespace std;
 vector<int> Finder::findSubstrings(string s1, string s2) {
   vector<int> result;
 
-  for (size_t i = 1; i <= s2.size(); i++) {
-    if(i>1 && result[i-1] == -1){
-        result.push_back(-1);
-    }
-    else{
-        size_t found = s1.find(s2.substr(0, i));
-            if (found != string::npos) {
-            result.push_back(found);
-            } else {
-                result.push_back(-1);
-            }
-    }
-    
-  }
-
-  return result;
-}
-/*for(size_t i = 0; i <= s1.size(); i++) {
+for(size_t i = 0; i <= s1.size(); i++) {
     int indexS1 = i;
     int indexS2 = 0;
     bool allFound = false;
+    if(s1[indexS1] == s2[indexS2] && result.size() > 0){
+        string compareStr1 = s1.substr(i, result.size());
+        string compareStr2 = s2.substr(0, result.size());
+        if (compareStr1 != compareStr2){
+            continue;
+        }
+        else{
+            indexS1 = indexS1 + result.size();
+            indexS2 = indexS2 + result.size();
+        }
+    }
     while(s1[indexS1] == s2[indexS2]){
         if(indexS2 + 1 > result.size()){
             result.push_back(i);
@@ -52,4 +46,4 @@ for (int j = 0; j < returnNotFound; j++){
 result.push_back(-1);
 }
 return result;
-}*/
+}
