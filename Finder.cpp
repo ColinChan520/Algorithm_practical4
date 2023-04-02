dadd16f80408adfb57154f07d8c903bd69b80475
ColinChan520 <a1864874@adelaide.edu.au>
2023-04-02 20:37:18 +0930

    5th commit

--- Finder.cpp
+++ Finder.cpp
@@ -6,13 +6,20 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
   vector<int> result;
 
   for (size_t i = 1; i <= s2.size(); i++) {
-    size_t found = s1.find(s2.substr(0, i));
-    if (found != string::npos) {
-      result.push_back(found);
-    } else {
-      result.push_back(-1);
+    if(i>1 && result[i-1] == -1){
+        result.push_back(-1);
     }
+    else{
+        size_t found = s1.find(s2.substr(0, i));
+            if (found != string::npos) {
+            result.push_back(found);
+            } else {
+                result.push_back(-1);
+            }
+    }
+    
   }
+
   return result;
 }
 /*for(size_t i = 0; i <= s1.size(); i++) {
